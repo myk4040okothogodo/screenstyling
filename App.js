b8fa6e207f35d0f76f1cfc791e8e229d2fd2ebd2
myk4040okothogodo <mikeogodo5@gmail.com>
2022-05-04 23:38:45 +0300

    curvedscreen

--- App.js
+++ App.js
@@ -3,9 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1}}>
+    <View style={{height:'100%', width:'100%', elevation:1, flexDirection:'column', alignItems:'stretch',justifyContent: 'center',position:'absolute'}}>
+      <View style={styles.view2}><Text>Them</Text></View>
+      <View style={styles.view1}><Text>Those</Text></View>
+    </View>  
+    <View style={{ height:'100%', width:'100%', flexDirection:'row' , alignItems:'stretch',justifyContent: 'center' }}>
+    <View style={styles.view1}><Text>This</Text></View>
+    <View style={styles.view2}><Text>That</Text></View>
+    </View>
     </View>
   );
 }
@@ -13,8 +19,31 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     backgroundColor: '#fff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  container2: {
+    flexDirection: 'column',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+    top:5,
+  },
+  view1 : {
+    flex: 1.8,
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
+    borderTopRightRadius: 120,
+  },
+  view2 : {
+     flex: 1,
+     backgroundColor: 'green',
+     alignItems: 'center',
+     justifyContent: 'center',
+    borderBottomLeftRadius: 50,
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
   },
 });
